8764949a1df7c156cfec9be0e9a35caeb9a0a0c9
Alexy <alexandra.fina@yahoo.com>
2018-03-02 15:16:19 +0000

    First test for turn function

---
+++ jasmine/spec/TicTacToeSpec.js
@@ -0,0 +1,13 @@
+describe('TicTacToe', function (){
+
+  describe('The Player can ', function(){
+    it('have a turn', function() {
+      player1 = new Player('Helen')
+      player2 = new Player('Dan')
+      game = new TicTacToe(player1, player2);
+      game.turn('x', 0);
+      expect(game.tic_array[0]).toEqual('x');
+    });
+
+  });
+  });
