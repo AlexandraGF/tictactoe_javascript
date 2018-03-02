de011929291f6a97ad4e98b35c309ddf895363fb
Alexy <alexandra.fina@yahoo.com>
2018-03-02 15:35:00 +0000

    Test that after turn function, available fields number is lowered

--- jasmine/spec/TicTacToeSpec.js
+++ jasmine/spec/TicTacToeSpec.js
@@ -1,13 +1,23 @@
 describe('TicTacToe', function (){
 
   describe('The Player can ', function(){
-    it('have a turn', function() {
+    it('have a turn and option is added to the array', function() {
       player1 = new Player('Helen')
       player2 = new Player('Dan')
       game = new TicTacToe(player1, player2);
       game.turn('x', 0);
       expect(game.ticArray[0]).toEqual('x');
     });
+
+   describe('The turn method ', function(){
+    it('lowers number of available fields in a game', function() {
+      player1 = new Player('Helen')
+      player2 = new Player('Dan')
+      game = new TicTacToe(player1, player2);
+      game.turn('x', 0);
+      expect(game.availableFields).toEqual(8);
+    });
+  });
 
   });
   });
--- jasmine/src/TicTacToe.js
+++ jasmine/src/TicTacToe.js
@@ -2,8 +2,10 @@ var TicTacToe = function (player1, player2){
   this.player1 = player1;
   this.player2 = player2;
   this.ticArray = ["", "", "", "", "", "", "", "", ""];
+  this.availableFields = 9;
 };
 
 TicTacToe.prototype.turn = function (option, position) {
   this.ticArray[position] = option
+  this.availableFields -= 1;
 };
