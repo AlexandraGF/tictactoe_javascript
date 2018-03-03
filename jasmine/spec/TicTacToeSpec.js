bcb5d53a176e42053170c1e985102a4fbf9d0064
Alexy <alexandra.fina@yahoo.com>
2018-03-03 18:26:59 +0000

    TicTacToe - test pass and business logic

--- jasmine/spec/TicTacToeSpec.js
+++ jasmine/spec/TicTacToeSpec.js
@@ -2,22 +2,71 @@ describe('TicTacToe', function (){
 
   describe('The Player can ', function(){
     it('have a turn and option is added to the array', function() {
-      player1 = new Player('Helen')
-      player2 = new Player('Dan')
+      player1 = new Player('Helen');
+      player2 = new Player('Dan');
       game = new TicTacToe(player1, player2);
       game.turn('x', 0);
       expect(game.ticArray[0]).toEqual('x');
     });
+  });
 
    describe('The turn method ', function(){
     it('lowers number of available fields in a game', function() {
-      player1 = new Player('Helen')
-      player2 = new Player('Dan')
+      player1 = new Player('Helen');
+      player2 = new Player('Dan');
       game = new TicTacToe(player1, player2);
       game.turn('x', 0);
       expect(game.availableFields).toEqual(8);
     });
+
+    it('fullLine method checks if 3 spaces in a row/column/diagonal have the same option x', function() {
+      player1 = new Player('Helen');
+      player2 = new Player('Dan');
+      game = new TicTacToe(player1, player2);
+      game.turn('x', 0);
+      game.turn('o', 1);
+      game.turn('x', 3);
+      game.turn('o', 4);
+      expect(game.turn('x', 6)).toEqual("Game over! Helen has won!");
+    });
+
+    it('full_line method checks if 3 spaces in a row/column/diagonal have the same option o', function() {
+      player1 = new Player('Helen');
+      player2 = new Player('Dan');
+      game = new TicTacToe(player1, player2);
+      game.turn('x', 0);
+      game.turn('o', 1);
+      game.turn('x', 3);
+      game.turn('o', 4);
+      game.turn('x', 5);
+      expect(game.turn('o', 7)).toEqual("Game over! Dan has won!");
+      });
+
+      it('available_space method checks if there is any available fields to play', function() {
+        player1 = new Player('Helen');
+        player2 = new Player('Dan');
+        game = new TicTacToe(player1, player2);
+        game.turn('x', 5);
+        game.turn('o', 4);
+        game.turn('x', 7);
+        game.turn('o', 3);
+        game.turn('x', 1);
+        game.turn('o', 2);
+        game.turn('x', 6);
+        game.turn('o', 8);
+        expect(game.turn('x', 0)).toEqual("Game over! No one won! Not available spaces to play");
+      });
+
+
+      it('player can not take a taken field', function() {
+        player1 = new Player('Helen');
+        player2 = new Player('Dan');
+        game = new TicTacToe(player1, player2);
+        game.turn('x', 0);
+        game.turn('o', 1);
+        game.turn('x', 3);
+        expect(game.turn('o', 0)).toEqual('Cannot choose a taken field! Please choose another empty field!')
+      });
   });
 
-  });
   });
--- jasmine/src/TicTacToe.js
+++ jasmine/src/TicTacToe.js
@@ -6,6 +6,88 @@ var TicTacToe = function (player1, player2){
 };
 
 TicTacToe.prototype.turn = function (option, position) {
-  this.ticArray[position] = option
+  if (this.ticArray[position] == 'x' || this.ticArray[position] == 'o') {
+    return this._takenFields();
+  }
+  else if (this.ticArray[position] != 'x' || this.ticArray[position] != 'o') {
+  this.ticArray[position] = option;
   this.availableFields -= 1;
+};
+
+  if (this.availableFields != 0) {
+    return this._fullLine();
+  }
+  else if (this.availableFields == 0) {
+    return this._gameOver();
+  };
+
+};
+
+
+
+TicTacToe.prototype._gameOver = function () {
+  if (this.availableFields == 0) {
+    return "Game over! No one won! Not available spaces to play";
+  }
+};
+
+
+
+TicTacToe.prototype._takenFields = function () {
+  return 'Cannot choose a taken field! Please choose another empty field!';
+};
+
+
+TicTacToe.prototype._fullLine = function () {
+
+if (this.ticArray[0] == 'x' && this.ticArray[3] == 'x' && this.ticArray[6] == 'x'){
+  return "Game over! " + this.player1.name + " has won!";
+}
+  if (this.ticArray[1] == 'x' && this.ticArray[4] == 'x' && this.ticArray[7] == 'x'){
+    return "Game over! " + this.player1.name + " has won!";
+  }
+  if (this.ticArray[2] == 'x' && this.ticArray[5] == 'x' && this.ticArray[8] == 'x'){
+    return "Game over! " + this.player1.name + " has won!";
+  }
+  if (this.ticArray[2] == 'x' && this.ticArray[4] == 'x' && this.ticArray[6] == 'x') {
+    return "Game over! " + this.player1.name + " has won!";
+  }
+  if (this.ticArray[0] == 'x' && this.ticArray[4] == 'x' && this.ticArray[8] == 'x') {
+    return "Game over! " + this.player1.name + " has won!";
+  }
+  if (this.ticArray[0] == 'x' && this.ticArray[1] == 'x' && this.ticArray[2] == 'x') {
+    return "Game over! " + this.player1.name + " has won!";
+  }
+  if (this.ticArray[3] == 'x' && this.ticArray[4] == 'x' && this.ticArray[5] == 'x') {
+    return "Game over! " + this.player1.name + " has won!";
+  }
+  if (this.ticArray[6] == 'x' && this.ticArray[7] == 'x' && this.ticArray[8] == 'x'){
+    return "Game over! " + this.player1.name + " has won!";
+  }
+
+
+  if (this.ticArray[0] == 'o' && this.ticArray[3] == 'o' && this.ticArray[6] == 'o'){
+    return "Game over! " + this.player2.name + " has won!";
+  }
+  if (this.ticArray[1] == 'o' && this.ticArray[4] == 'o' && this.ticArray[7] == 'o'){
+    return "Game over! " + this.player2.name + " has won!";
+  }
+  if (this.ticArray[2] == 'o' && this.ticArray[5] == 'o' && this.ticArray[8] == 'o'){
+    return "Game over! " + this.player2.name + " has won!";
+  }
+  if (this.ticArray[2] == 'o' && this.ticArray[4] == 'o' && this.ticArray[6] == 'o'){
+    return "Game over! " + this.player2.name + " has won!";
+  }
+  if (this.ticArray[0] == 'o' && this.ticArray[4] == 'o' && this.ticArray[8] == 'o'){
+    return "Game over! " + this.player2.name + " has won!";
+  }
+  if (this.ticArray[0] == 'o' && this.ticArray[1] == 'o' && this.ticArray[2] == 'o'){
+    return "Game over! " + this.player2.name + " has won!";
+  }
+  if (this.ticArray[3] == 'o' && this.ticArray[4] == 'o' && this.ticArray[5] == 'o'){
+    return "Game over! " + this.player2.name + " has won!";
+  }
+  if (this.ticArray[6] == 'o' && this.ticArray[7] == 'o' && this.ticArray[8] == 'o'){
+  return "Game over! " + this.player2.name + " has won!";
+}
 };
